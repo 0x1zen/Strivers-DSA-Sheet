7d120cbc88515d2c69c1a1d086aa6b9a6d055e72
0x1zen <rajdubalwork@gmail.com>
2024-09-19 23:33:14 +0530

    Day 1 Stack And Queues

---
+++ Stacks And Queues/0.stackUsingArray.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<limits.h>
+#include<set>
+#include<string.h>
+#include<algorithm>
+using namespace std;
+class stImpl{
+    int st[10];
+    int top;
+
+    public:
+    stImpl(){
+        top=-1;
+    }
+    void push(int x){
+        if(top>=10){
+            cout<<"Max Size Reached";
+        }
+        top=top+1;
+        st[top]=x;
+    };
+    void pop(){
+        if(top==-1){
+            cout<<"No Value present";
+        }
+        top=top-1;
+
+    };
+    int size(){
+        return top+1;
+    };
+    int getTop(){
+        if(top==-1){
+            cout<<"No value present";
+        }
+        return st[top];
+    };
+};
+int main()
+{
+    stImpl st;
+    st.push(10);
+    cout<<"size before";
+    cout<<st.size();
+    cout<<"size after";
+    st.pop();
+    cout<<st.size();
+    st.push(30);
+    st.push(10);
+    cout<<st.getTop();
+ return 0;
+}
